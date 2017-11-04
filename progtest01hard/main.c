94ead66a6cac3068a00873aca6aebcb264d925ef
Tadeáš Valenta <tadashicz@gmail.com>
2017-11-04 20:49:00 +0100

    version 1.0, added test samples, complete gitignore

--- progtest01hard/main.c
+++ progtest01hard/main.c
@@ -76,10 +76,60 @@ int nacti ( double* Ax, double* Ay, double* Bx, double* By, double* Cx, double* 
         return 0;
     }
 
-    int findFourthPoint(double Ax, double Ay, double Bx, double By, double Cx, double Cy, double* solutionX, double* solutionY){
+    void findFourthPoint(double Ax, double Ay, double Bx, double By, double Cx, double Cy, double* solutionX, double* solutionY){
         *solutionX = Bx+(Cx-Ax);
         *solutionY = By+(Cy-Ay);
     }
+
+int isSameEnough(double x, double y){
+   // if (fabs(x - y) < DBL_EPSILON) return 1;
+    double relativeError;
+    (fabs(y) > fabs(x)) ? relativeError = fabs((x - y) / y) : relativeError = fabs((x - y) / x);
+
+    if (relativeError <= 0.000000001)
+
+        return 1;
+
+    return 0;
+}
+
+int whatShapeItIs(double Ax, double Ay, double Bx, double By, double Cx, double Cy, double Dx, double Dy){
+    double sizeOfSide1 = sqrt( pow( fabs(Ax-Bx), 2) + pow( (Ay-By), 2) );
+    double sizeOfSide2 = sqrt( pow( fabs(Bx-Cx), 2) + pow( (By-Cy), 2) );
+    double sizeOfSide3 = sqrt( pow( fabs(Cx-Dx), 2) + pow( (Cy-Dy), 2) );
+    double sizeOfSide4 = sqrt( pow( fabs(Dx-Ax), 2) + pow( (Dy-Ay), 2) );
+
+    double sizeOfDiagonal1 = sqrt( pow( fabs(Ax-Cx), 2) + pow( (Ay-Cy), 2) );
+    double sizeOfDiagonal2 = sqrt( pow( fabs(Bx-Dx), 2) + pow( (By-Dy), 2) );
+
+    if (isSameEnough(sizeOfSide1, sizeOfSide2) &&
+        isSameEnough(sizeOfSide2, sizeOfSide3) &&
+        isSameEnough(sizeOfSide3, sizeOfSide4) &&
+        isSameEnough(sizeOfSide4, sizeOfSide1) &&
+        isSameEnough(sizeOfSide1, sizeOfSide3) &&
+        isSameEnough(sizeOfSide2, sizeOfSide4)){
+
+        if (isSameEnough(sizeOfDiagonal1, sizeOfDiagonal2)) return 1;
+        return 2;
+    }
+
+    if (isSameEnough(sizeOfSide1, sizeOfSide3) && isSameEnough(sizeOfSide2, sizeOfSide4)){
+        if (isSameEnough(sizeOfDiagonal1, sizeOfDiagonal2)) return 3;
+        return 4;
+    }
+/*
+    printf("Je AB a BC stejné? %d\n", isSameEnough(sizeOfSide1, sizeOfSide2));
+    printf("Je BC a DA stejné? %d\n", isSameEnough(sizeOfSide2, sizeOfSide4));
+    printf("Strana A-B: %.10f\n", sizeOfSide1);
+    printf("Strana B-C: %.10f\n", sizeOfSide2);
+    printf("Strana C-D: %.10f\n", sizeOfSide3);
+    printf("Strana D-A: %.10f\n", sizeOfSide4);
+    printf("Úhlopříčka A-C: %.10f\n", sizeOfDiagonal1);
+    printf("Úhlopříčka B-D: %.10f\n\n", sizeOfDiagonal2);
+*/
+
+    return 1;
+}
 
 
     int main() {
@@ -92,15 +142,7 @@ int nacti ( double* Ax, double* Ay, double* Bx, double* By, double* Cx, double* 
             return 1;
         }
 
-        if ( (1.0 + DBL_EPSILON) != 1.0 && (1.0 + DBL_EPSILON/2) == 1.0 ){
-            printf("epsilon funguje\n");
-            printf("%.100f\n",(Ax+Bx)*DBL_EPSILON);
-        }
-
-        if ( fabs(Ax * (By - Cy) + Bx * (Cy - Ay) + Cx * (Ay - By) ) < DBL_EPSILON ){
-            printf("By-Cy: %.100f\n", By-Cy);
-            printf("Cy-Ay: %.100f\n", Cy-Ay);
-            printf("Ay-By: %.100f\n", Ay-By);
+        if ( fabs( Ax * (By - Cy) + Bx * (Cy - Ay) + Cx * (Ay - By) ) < DBL_EPSILON ){
             printf("Rovnobezniky nelze sestrojit.\n");
             return 1;
         }
@@ -109,16 +151,52 @@ int nacti ( double* Ax, double* Ay, double* Bx, double* By, double* Cx, double* 
         findFourthPoint(Bx, By, Cx, Cy, Ax, Ay, &solution2x, &solution2y);
         findFourthPoint(Cx, Cy, Ax, Ay, Bx, By, &solution3x, &solution3y);
 
+        printf("A': [%.6g,%.6g], ", solution1x, solution1y);
+        switch (whatShapeItIs(Ax,Ay,Bx,By,solution1x,solution1y,Cx,Cy) ){
+            case 1: printf("ctverec\n");
+                    break;
+            case 2: printf("kosoctverec\n");
+                    break;
+            case 3: printf("obdelnik\n");
+                break;
+            case 4: printf("rovnobeznik\n");
+                break;
+        }
+
+        printf("B': [%.6g,%.6g], ", solution2x, solution2y);
+        switch(whatShapeItIs(Ax,Ay,Bx,By,Cx,Cy,solution2x,solution2y)) {
+            case 1: printf("ctverec\n");
+                break;
+            case 2: printf("kosoctverec\n");
+                break;
+            case 3: printf("obdelnik\n");
+                break;
+            case 4: printf("rovnobeznik\n");
+                break;
+        }
+
+        printf("C': [%.6g,%.6g], ", solution3x, solution3y);
+        switch ( whatShapeItIs(Ax,Ay,solution3x,solution3y,Bx,By,Cx,Cy) ){
+            case 1: printf("ctverec\n");
+                break;
+            case 2: printf("kosoctverec\n");
+                break;
+            case 3: printf("obdelnik\n");
+                break;
+            case 4: printf("rovnobeznik\n");
+                break;
+        }
 
 
 
-        printf("Bod A: [%g, %g]\n", Ax, Ay);
-        printf("Bod B: [%g, %g]\n", Bx, By);
-        printf("Bod C: [%g, %g]\n", Cx, Cy);
 
-        printf("Solution A: [%g, %g]\n", solution1x, solution1y);
-        printf("Solution B: [%g, %g]\n", solution2x, solution2y);
-        printf("Solution C: [%g, %g]\n", solution3x, solution3y);
+        // printf("Bod A: [%.10f, %.10f]\n", Ax, Ay);
+        // printf("Bod B: [%.10f, %.10f]\n", Bx, By);
+        // printf("Bod C: [%.10f, %.10f]\n", Cx, Cy);
+
+        // printf("Solution A: [%.10f, %.10f]\n", solution1x, solution1y);
+        // printf("Solution B: [%.10f, %.10f]\n", solution2x, solution2y);
+        // printf("Solution C: [%.10f, %.10f]\n", solution3x, solution3y);
 
         return 0;
-    };
+    }
