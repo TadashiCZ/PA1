7038bdee81e9aa744a8e47504c06fda29c606a79
Tadeáš Valenta <tadashicz@gmail.com>
2017-12-10 22:35:40 +0100

    progtest06hard wip

---
+++ progtest06hard/main.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <ctype.h>
+
+char* clearDelimiters(char* input){
+    char* output = (char*) malloc(strlen(input) * sizeof(char));
+    int counter = 0, j=0;
+    for (int i = 0 ;  ; i++){
+        if (input[i] == '\0'){
+            output[counter] = '\0';
+            break;
+        }
+       // printf("Odmazávám zbytečnosti.\n");
+            if (isalpha(input[i]) || input[i] == ' ' || input[i] == '\n'){
+                output[counter] = input[i];
+                printf("%c", output[counter]);
+                counter++;
+            }
+    }
+    for (int i = 0 ; i < strlen(output) ; i++){
+       // printf("Dávám pryč mezery.\n");
+        if (output[i] == ' '){
+            j = i+1;
+            while(output[j] == ' '){
+                output[j]=1;
+                j++;
+            }
+        }
+    }
+
+    return output;
+}
+
+
+int main() {
+    long long int position=0, max = 100, questionPosition=0;
+    char* input = NULL;
+    char* clearedInput = NULL;
+  //  char* lookForThis = NULL;
+    char* questionInput = NULL;
+    int charInput, endInput = 0, lastQuestion = 0, firstSign = 0;
+    printf("Text:\n");
+    input = (char*) malloc(max * sizeof(char));
+
+
+    while (1){
+        charInput = getchar();
+
+        if (charInput == EOF ) {
+            free (input);
+            printf("Nespravny vstup.\n");
+            return 1;
+        }
+
+        if ((char) charInput == '\n') {
+            endInput++;
+        } else {
+            endInput = 0;
+        }
+
+        if ( position >= max-1 ){
+                max += ( max < 1000 ) ? 200 : max / 2;
+                input = (char*) realloc(input, max * sizeof(char));
+        }
+
+        input[position] = (char)charInput;
+        position++;
+
+
+        if ( endInput == 2 && (charInput == '\n') )  break;
+    }
+    for (int i = 0 ; i <= position ; i++){
+        input[i] = (char)tolower(input[i]);
+    }
+
+    clearedInput = clearDelimiters(input);
+    free(input);
+    printf("Výpis:\n%s\n------\n",clearedInput);
+
+    printf("Hledani:\n");
+
+    max = 100;
+    questionInput = (char*) malloc(max * sizeof(char));
+    while (1){
+        questionPosition = 0;
+        firstSign = 1;
+
+        for (int i = 0 ; ; i++) {
+            if ( questionPosition >= max-1 ){
+                max += ( max < 1000 ) ? 200 : max / 2;
+                questionInput = (char*) realloc(questionInput, max * sizeof(char));
+            }
+
+            charInput = (char) getchar();
+            if (charInput == '\n' || charInput == EOF ) {
+                questionInput[i] = '\0';
+                break;
+            }
+            questionInput[i] = (char) charInput;
+            questionPosition++;
+            firstSign = 0;
+        }
+        if (firstSign != 1) questionInput = clearDelimiters(questionInput);
+        printf("Question input: %s\n", questionInput);
+        if (charInput == EOF) {
+            lastQuestion = 1;
+            break;
+        }
+
+        if (lastQuestion){
+            printf("Proběhl last question.\n");
+            break;
+        }
+    }
+
+
+   // printf("%c", *strstr(input, "morbi"));
+    free(clearedInput);
+    free(questionInput);
+    return 0;
+}
