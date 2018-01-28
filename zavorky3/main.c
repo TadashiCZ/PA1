6151e59c7910596a0853105679303a4bf6c52809
Tadeáš Valenta <tadashicz@gmail.com>
2018-01-28 21:37:46 +0100

    funkční verze s funkcemi pro dva a tři typy a pro dva typy se suffixem

--- zavorky3/main.c
+++ zavorky3/main.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
+#include <memory.h>
 
-unsigned long int catalanDP(unsigned int n) {
-    // Table to store results of subproblems
-    unsigned long int catalan[n + 1];
-
-    // Initialize first two values in table
-    catalan[0] = catalan[1] = 1;
-
-    // Fill entries in catalan[] using recursive formula
-    for (int i = 2; i <= n; i++) {
-        catalan[i] = 0;
-        for (int j = 0; j < i; j++)
-            catalan[i] += catalan[j] * catalan[i - j - 1];
-    }
-    // Return last entry
-    return catalan[n];
-}
-
-long long combi(int n, int k) {
+long long combi(long long int n, long long int k) {
     long long ans = 1;
     if (k > n - k)
         k = n - k;
-    
+
     int i = 1;
     for (; i <= k; i++, n--) {
         if (n % i == 0) {
@@ -38,9 +22,38 @@ long long combi(int n, int k) {
 
 
 long long int countAll(int n, int m) {
-    long long int result = (combi(2*n+2*m, n+m) * combi(n+m, n))  / (n+m+1);
-    printf("%lld\n", result);
+    long long int result = (combi(2 * n + 2 * m, n + m) * combi(n + m, n)) / (n + m + 1);
+    printf("result of two: %lld\n", result);
     return result;
+}
+
+long long int countAllThree(int m, int n, int o) {
+    long long int result =
+            (combi(2 * m + 2 * n + 2 * o, m + n + o) * combi(m + n + o, m) * combi(n + o, o)) / (n + m + o + 1);
+    printf("result of three: %lld\n", result);
+    return result;
+}
+
+long long int countTwoWithSuffix(int n, int m, const char* suffix){
+    long long int countN = 0, countM = 0;
+    for (unsigned int i = 0 ; i < strlen(suffix) ; i++){
+        if (suffix[i] == '(' && suffix[i+1] == ')'){
+            n--;
+            i++;
+        } else if (suffix[i]  == '[' && suffix[i+1]  == ']'){
+            m--;
+            i++;
+        } else if (suffix[i] == ')'){
+            countN++;
+        } else if (suffix[i]  == ']' ){
+            countM++;
+        }
+    }
+    long long int result = (combi(2 * n + 2 * m, n + m) * combi(n + m - (countN+countM), n-countM)) / (n + m + 1);
+    printf("result of two with suffix: %lld\n", result);
+    return result;
+
+
 }
 
 int main() {
@@ -53,7 +66,7 @@ int main() {
     assert(countAll(0, 7) == 429);
     assert(countAll(1, 0) == 1);
     assert(countAll(0, 0) == 1);
-
-
+    assert(countTwoWithSuffix(1,2,"[]") == 4); //nefunguje, protoze suffixova funkce nefunguje
+    assert(countAllThree(2,1,1) == 168);
     return 0;
 }
