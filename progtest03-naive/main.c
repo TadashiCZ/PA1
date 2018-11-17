8e38c3757cec2e1f30d76ee1cfbe3db40b6b378e
TadashiCZ <tadashicz@gmail.com>
2018-11-17 23:14:19 +0100

    progtest03 naive for 5.5

--- progtest03-naive/main.c
+++ progtest03-naive/main.c
@@ -7,35 +7,73 @@
 
 #endif /* __PROGTEST__ */
 
-unsigned long long int
-count3(unsigned long long int length, unsigned int s1, unsigned int s2, unsigned int s3,
-       unsigned int * c1, unsigned int * c2, unsigned int * c3) {
-    return 0;
+void inputSegment(unsigned long long int segment1, unsigned long long int segment2, unsigned long long int segment3,
+                  unsigned int * c1, unsigned int * c2, unsigned int * c3) {
+    *c1 = (unsigned int) segment1;
+    *c2 = (unsigned int) segment2;
+    *c3 = (unsigned int) segment3;
 }
 
 unsigned long long int
-count2(unsigned long long int length, unsigned int s1, unsigned int s2,
-       unsigned int * c1, unsigned int * c2, unsigned int * c3) {
+hyperloop3(unsigned long long int length, unsigned int s1, unsigned int s2, unsigned int s3,
+           unsigned int * c1, unsigned int * c2, unsigned int * c3) {
     unsigned long long int segment1 = 0, segment2 = 0, segment3 = 0, found = 0;
     unsigned long long int count = 0, currentLength = 0;
 
-    while ( currentLength < length ) {
+    while ( currentLength <= length ) {
 
-        segment1 = (length - currentLength) / s1;
-        if ((length - currentLength) == segment1 * s1) {
-            
+        while ( currentLength <= length ) {
+            if ((length - currentLength) % s1 == 0) {
+                segment1 = (length - currentLength) / s1;
+                count++;
+                if (!found) {
+                    found = 1;
+                    inputSegment(segment1, segment2, segment3, c1, c2, c3);
+                }
+            }
 
+            segment2++;
+            currentLength += s2;
         }
 
+        currentLength = currentLength - segment2 * s2;
+        segment2 = 0;
+
+        segment3++;
+        currentLength += s3;
+    }
+
+    return count;
+}
+
+unsigned long long int
+hyperloop2(unsigned long long int length, unsigned int s1, unsigned int s2,
+           unsigned int * c1, unsigned int * c2, unsigned int * c3) {
+
+    unsigned long long int segment1 = 0, segment2 = 0, segment3 = 0, found = 0;
+    unsigned long long int count = 0, currentLength = 0;
+
+    while ( currentLength <= length ) {
+
+
+        if ((length - currentLength) % s1 == 0) {
+            segment1 = (length - currentLength) / s1;
+            count++;
+            if (!found) {
+                found = 1;
+                inputSegment(segment1, segment2, segment3, c1, c2, c3);
+            }
+        }
+
+        segment2++;
         currentLength += s2;
     }
 
-
-    return 0;
+    return count;
 }
 
-unsigned long long int count1(unsigned long long int length, unsigned int s1,
-                              unsigned int * c1, unsigned int * c2, unsigned int * c3) {
+unsigned long long int hyperloop1(unsigned long long int length, unsigned int s1,
+                                  unsigned int * c1, unsigned int * c2, unsigned int * c3) {
     if (length % s1 == 0) {
         *c1 = length / s1;
         *c2 = 0;
@@ -50,22 +88,61 @@ unsigned long long int count1(unsigned long long int length, unsigned int s1,
 unsigned long long int
 hyperloop(unsigned long long int length, unsigned int s1, unsigned int s2, unsigned int s3, unsigned int bulkhead,
           unsigned int * c1, unsigned int * c2, unsigned int * c3) {
+
     if (bulkhead > length) return 0;
+
+
+    if (bulkhead == 0 && length == 0){
+        return 0;
+    }
+
+   if (bulkhead == length) {
+        *c1 = 0;
+        *c2 = 0;
+        *c3 = 0;
+        return 1;
+    }
+
+    if (s1 == 0 && s2 == 0 && s3 == 0){
+        return 0;
+    }
+
     if (length == 0) return 0;
 
-    if (s1 == s2 && s2 == s3 && s1 == s3) {
-        return count1(length - bulkhead, s1 + bulkhead, c1, c2, c3);
-    }
-
     if (s1 == s2) {
-        return count2(length - bulkhead, s1 + bulkhead, s2 + bulkhead, c1, c2, c3);
-    } else if (s2 == s3) {
-        return count2(length - bulkhead, s2 + bulkhead, s3 + bulkhead, c2, c3, c1);
-    } else if (s1 == s3) {
-        return count2(length - bulkhead, s1 + bulkhead, s3 + bulkhead, c1, c2, c2);
+        s2 = 0;
     }
 
-    return count3(length - bulkhead, s1 + bulkhead, s2 + bulkhead, s3 + bulkhead, c1, c2, c3);
+    if (s1 == s3) {
+        s3 = 0;
+    }
+
+    if (s2 == s3) {
+        s3 = 0;
+    }
+
+
+    if (s1 && !s2 && !s3) {
+        return hyperloop1(length - bulkhead, s1 + bulkhead, c1, c2, c3);
+    }
+
+    if (!s1 && s2 && !s3) {
+        return hyperloop1(length - bulkhead, s2 + bulkhead, c2, c1, c3);
+    }
+
+    if (!s1 && !s2 && s3) {
+        return hyperloop1(length - bulkhead, s3 + bulkhead, c3, c2, c1);
+    }
+
+    if (s1 && s2 && !s3) {
+        return hyperloop2(length - bulkhead, s1 + bulkhead, s2 + bulkhead, c1, c2, c3);
+    } else if (!s1 && s2 && s3) {
+        return hyperloop2(length - bulkhead, s2 + bulkhead, s3 + bulkhead, c2, c3, c1);
+    } else if (s1 && !s2 && s3) {
+        return hyperloop2(length - bulkhead, s1 + bulkhead, s3 + bulkhead, c1, c3, c2);
+    }
+
+    return hyperloop3(length - bulkhead, s1 + bulkhead, s2 + bulkhead, s3 + bulkhead, c1, c2, c3);
 
 
 }
@@ -74,6 +151,71 @@ hyperloop(unsigned long long int length, unsigned int s1, unsigned int s2, unsig
 
 int main(int argc, char * argv[]) {
     unsigned int c1, c2, c3;
+
+
+
+    assert ( hyperloop ( 226, 0, 0, 0, 226, &c1, &c2, &c3 ) == 1 && 0 * c1 + 0 * c2 + 0 * c3 + 226 * ( c1 + c2 + c3 + 1 ) == 226 );
+    assert ( hyperloop ( 226, 226, 0, 0, 0, &c1, &c2, &c3 ) == 1 && 226 * c1 + 0 * c2 + 0 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 226 );
+    assert ( hyperloop ( 226, 0, 226, 0, 0, &c1, &c2, &c3 ) == 1 && 0 * c1 + 226 * c2 + 0 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 226 );
+    assert ( hyperloop ( 226, 0, 0, 226, 0, &c1, &c2, &c3 ) == 1 && 0 * c1 + 0 * c2 + 226 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 226 );
+    assert ( hyperloop ( 226, 0, 226, 226, 0, &c1, &c2, &c3 ) == 1 && 0 * c1 + 226 * c2 + 226 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 226 );
+    assert ( hyperloop ( 226, 226, 0, 226, 0, &c1, &c2, &c3 ) == 1 && 226 * c1 + 0 * c2 + 226 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 226 );
+    assert ( hyperloop ( 226, 226, 226, 226, 0, &c1, &c2, &c3 ) == 1 && 226 * c1 + 226 * c2 + 226 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 226 );
+    c1 = 42; c2 = 43; c3 = 44;
+    assert ( hyperloop ( 9, 0, 0, 0, -1, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( -9, 0, 0, 3, 0, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 0, 0, 0, 0, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 0, 0, 0, 3, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 12, 0, 0, 0, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 0, 12, 0, 0, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 0, 0, 12, 0, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 0, 0, 0, 12, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 12, 12, 12, 12, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 12, 0, 12, 0, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 0, 12, 12, 0, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 12, 12, 0, 0, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 0, 0, 12, 12, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 0, 12, 0, 12, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 12, 0, 0, 12, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 12, 0, 0, 12, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 4, 0, 0, 0, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 4, 0, 0, 3, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 0, 0, 0, 0, 0, &c1, &c2, &c3 ) == 0  && c1 == 42 && c2 == 43 && c3 == 44);
+    assert ( hyperloop ( 0, 4, 5, 2, 7, &c1, &c2, &c3 ) == 0 && c1 == 42 && c2 == 43 && c3 == 44 );
+    assert ( hyperloop ( 9, 3, 2, 0, 0, &c1, &c2, &c3 ) == 2 && 3 * c1 + 2 * c2 + 0 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 9 );
+    assert ( hyperloop ( 9, 3, 2, 14, 0, &c1, &c2, &c3 ) == 2 && 3 * c1 + 2 * c2 + 0 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 9 );
+    assert ( hyperloop ( 9, 3, 3, 3, 0, &c1, &c2, &c3 ) == 1 && 3 * c1 + 3 * c2 + 3 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 9 );
+    assert ( hyperloop ( 9, 1, 1, 1, 0, &c1, &c2, &c3 ) == 1 && 1 * c1 + 1 * c2 + 1 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 9 );
+    assert ( hyperloop ( 99999999, 1, 1, 1, 0, &c1, &c2, &c3 ) == 1 && 1 * c1 + 1 * c2 + 1 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 99999999 );
+    assert ( hyperloop ( 99999999, 99999997, 0, 0, 1, &c1, &c2, &c3 ) == 1 && 99999997 * c1 + 0 * c2 + 0 * c3 + 1 * ( c1 + c2 + c3 + 1 ) == 99999999 );
+    assert ( hyperloop ( 1869, 623, 1246, 1246, 0, &c1, &c2, &c3 ) == 2 && 623 * c1 + 1246 * c2 + 1246 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 1869 );
+    assert ( hyperloop ( 1869, 1246, 623, 1246, 0, &c1, &c2, &c3 ) == 2 && 1246 * c1 + 623 * c2 + 1246 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 1869 );
+    assert ( hyperloop ( 1869, 1246, 1246, 623, 0, &c1, &c2, &c3 ) == 2 && 1246 * c1 + 1246 * c2 + 623 * c3 + 0 * ( c1 + c2 + c3 + 1 ) == 1869 );
+
+
+    assert (hyperloop ( 2007, 1338, 1338, 669, 0, &c1, &c2, &c3 ) == 2 && 1338 * c1 + 1338 * c2 + 669 * c3 + 0 * (c1 + c2 + c3 + 1) == 2007);
+
+    assert(hyperloop(1, 0, 0, 1, 1, &c1, &c2, &c3) == 1 && c1 == 0 && c2 == 0 && c3 == 0);
+    assert (hyperloop(573, 0, 191, 382, 0, &c1, &c2, &c3) == 2);
+    assert(hyperloop(226, 0, 0, 0, 226, &c1, &c2, &c3) == 1);
+
+
+    assert (hyperloop(226, 0, 0, 0, 226, &c1, &c2, &c3) == 1 &&
+            0 * c1 + 0 * c2 + 0 * c3 + 226 * (c1 + c2 + c3 + 1) == 226);
+    assert (hyperloop(226, 226, 0, 0, 0, &c1, &c2, &c3) == 1 &&
+            226 * c1 + 0 * c2 + 0 * c3 + 0 * (c1 + c2 + c3 + 1) == 226);
+    assert (hyperloop(226, 0, 226, 0, 0, &c1, &c2, &c3) == 1 &&
+            0 * c1 + 226 * c2 + 0 * c3 + 0 * (c1 + c2 + c3 + 1) == 226);
+    assert (hyperloop(226, 0, 0, 226, 0, &c1, &c2, &c3) == 1 &&
+            0 * c1 + 0 * c2 + 226 * c3 + 0 * (c1 + c2 + c3 + 1) == 226);
+    assert (hyperloop(226, 0, 226, 226, 0, &c1, &c2, &c3) == 1 &&
+            0 * c1 + 226 * c2 + 226 * c3 + 0 * (c1 + c2 + c3 + 1) == 226);
+    assert (hyperloop(226, 226, 0, 226, 0, &c1, &c2, &c3) == 1 &&
+            226 * c1 + 0 * c2 + 226 * c3 + 0 * (c1 + c2 + c3 + 1) == 226);
+    assert (hyperloop(226, 226, 226, 226, 0, &c1, &c2, &c3) == 1 &&
+            226 * c1 + 226 * c2 + 226 * c3 + 0 * (c1 + c2 + c3 + 1) == 226);
+
+
     assert (hyperloop(100, 4, 7, 5, 0, &c1, &c2, &c3) == 42
             && 4 * c1 + 7 * c2 + 5 * c3 + 0 * (c1 + c2 + c3 + 1) == 100);
     assert (hyperloop(12345, 8, 3, 11, 3, &c1, &c2, &c3) == 82708
@@ -103,6 +245,7 @@ int main(int argc, char * argv[]) {
             && c1 == 42
             && c2 == 43
             && c3 == 44);
+
     return 0;
 }
 
